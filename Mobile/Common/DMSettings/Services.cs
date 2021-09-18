cbda81eb175796bc45b27ae51164a38a408a0a87
Yuri Vasilyev <yuravas.dm@outlook.com>
2021-09-18 03:25:01 +0300

    append missed file

---
+++ Mobile/Common/DMSettings/Services.cs
@@ -0,0 +1,18 @@
+﻿namespace DMSettings
+{
+    public class Services
+    {
+        public const string InfoPanelUrl = @"http://infopanelserviceus.azurewebsites.net";
+        public const string PaymentUrl = @"http://paymentserviceus.azurewebsites.net";
+        public const string CashbackUrl = @"http://cashbackserviceus.azurewebsites.net";
+        public const string ACHPaymentUrl = @"http://achpaymentserviceus.azurewebsites.net";
+        public const string RouteEngineUrl = @"http://routeengineserviceus.azurewebsites.net";
+        public const string FlexibleFeeUrl = @"http://flexiblefeeserviceus.azurewebsites.net";
+        public const string ProcessingUrl = @"http://processlogicserviceus.azurewebsites.net";
+        public const string NetworkingUrl = @"http://networkingserviceus.azurewebsites.net/";
+        /// <summary>
+        /// Diesel.Market Payment notification service
+        /// </summary>
+        public const string ExternalListenerUrl = "https://externallistenerus.azurewebsites.net/";
+    }
+}
